d56d3586522be3b3238ea7152204023713334417
VimalJ5 <vimalj2004.5@gmail.com>
2024-09-04 21:06:44 +0530

    fbref scraping

--- attack_scrapping.py
+++ attack_scrapping.py
@@ -2,13 +2,22 @@ import requests
 from bs4 import BeautifulSoup
 import pandas as pd
 
-pg = 1
-main_url = 'https://www.fifaindex.com/players/fifa23_589/?gender=0&league=13&order=desc'
+url = 'https://fbref.com/en/comps/9/2023-2024/stats/2023-2024-Premier-League-Stats'
+response = requests.get(url)
+soup = BeautifulSoup(response.text, 'html.parser')
+html_content = soup.prettify()
+uncommented_html = html_content.replace("<!--", "").replace("-->", "")
+soup_uncommented = BeautifulSoup(uncommented_html, "html.parser")
+table = soup_uncommented.find("table", {"id": "stats_standard"})
+print(table)
 
-response = requests.get(main_url)
-soup = BeautifulSoup(response.text, 'lxml')
-print(soup.prettify())
-
-# table = soup.find('table', {'class': 'table table-striped table-players'})
-# rows = table.find('tbody').find_all('tr')
-# print(type(table))
+offensive_position = {'FW','MF'}
+filtered_rows = []
+count = 0
+for row in table.find('tbody').find_all('tr'):
+    if "thead" in row.get("class", []):
+        continue
+    else:
+        count += 1
+        filtered_rows.append(row)
+print(count)
